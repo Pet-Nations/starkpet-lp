74ad9bc9c7a45a16c7015d2537a13d209a6df783
ssm-namdd <namdd@saseeme.com>
2024-07-27 10:08:14 +0700

    NamDD.SP-984/feat/add-title-and-desc fix style about us

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import Image from "next/image";
-import { useRef, useEffect, useState } from "react";
+import { useState } from "react";
+import "./globals.css";
 
 import HeaderBar from "@/app/components/HeaderBar";
 import Strategic from "@/app/components/Strategic";
