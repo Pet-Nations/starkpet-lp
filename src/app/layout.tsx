9ab1e6becdb1d80268a756783107e54777100bdd
ssm-namdd <namdd@saseeme.com>
2024-07-27 11:12:45 +0700

    NamDD.SP-984/feat/add-title-and-desc add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,11 +2,15 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import React from "react";
+import Head from "next/head";
 
 export const metadata: Metadata = {
   title: "Pet Labs by Pet Nations",
   description:
     "Lab, build all productions from Pet Nations. Ensure the stability of Pet Nations ecosystem",
+  icons: {
+    icon: "/favicon.ico",
+  },
 };
 
 const inter = Inter({ subsets: ["latin"] });
@@ -17,7 +21,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
@@ -32,7 +37,7 @@ export default function RootLayout({
           href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:ital,wght@0,200..800;1,200..800&display=swap"
           rel="stylesheet"
         />
-      </head>
+      </Head>
       <body className={inter.className + " container-stark"}>{children}</body>
     </html>
   );
