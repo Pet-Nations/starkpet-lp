325fee0220c68c19f50986398de26ba6664410f0
TienVinh <s2gali2s@gmail.com>
2024-07-30 17:36:02 +0700

    tienmvv.Fix error build 3:19 30/7/2024

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,17 @@
+"use client";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import React from "react";
-import Head from "next/head";
 
-export const metadata: Metadata = {
-  title: "Pet Labs by Pet Nations",
-  description:
-    "Lab, build all productions from Pet Nations. Ensure the stability of Pet Nations ecosystem",
-  icons: {
-    icon: "/favicon.ico",
-  },
-};
+// export const metadata: Metadata = {
+//   title: "Pet Labs by Pet Nations",
+//   description:
+//     "Lab, build all productions from Pet Nations. Ensure the stability of Pet Nations ecosystem",
+//   icons: {
+//     icon: "/favicon.ico",
+//   },
+// };
 
 const inter = Inter({ subsets: ["latin"] });
 export default function RootLayout({
@@ -21,7 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <Head>
+      <head>
         <link rel="icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
@@ -40,7 +40,7 @@ export default function RootLayout({
         <meta property="og:image" content="/petlabs_02.jpg" />
         <meta property="og:image:width" content="640" />
         <meta property="og:image:height" content="640" />
-      </Head>
+      </head>
       <body className={inter.className + " container-stark"}>{children}</body>
     </html>
   );
