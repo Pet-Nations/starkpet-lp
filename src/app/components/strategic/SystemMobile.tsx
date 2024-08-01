fcd3e8f8006641ad02cf895a0206b8fed3c37a4f
TienVinh <s2gali2s@gmail.com>
2024-08-01 11:14:08 +0700

    tienmvv.IN-1019 [StarkPet] Animation button on Mobile

--- src/app/components/strategic/SystemMobile.tsx
+++ src/app/components/strategic/SystemMobile.tsx
@@ -17,7 +17,7 @@ export default function SystemMobile() {
                             d="M16.0037 9.41421L7.39712 18.0208L5.98291 16.6066L14.5895 8H7.00373V6H18.0037V17H16.0037V9.41421Z"
                             fill="white"/>
                     </svg>
-                    Architect model
+                    <p>Architect model</p>
                 </button>
             </div>
         </div>
