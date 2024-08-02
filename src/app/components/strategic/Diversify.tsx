b535d1b0b01722e8d9cadaedddd5bb612d4ca371
TienVinh <s2gali2s@gmail.com>
2024-08-02 14:19:00 +0700

    tienmvv.Update link document diversify models

--- src/app/components/strategic/Diversify.tsx
+++ src/app/components/strategic/Diversify.tsx
@@ -12,7 +12,7 @@ export default function Diversify() {
                     <span>Diversify Models</span>
                     <p>Pet Nations Lab build sustainable models to ensure the ecosystem growth.</p>
                 </div>
-                <a className="diversify-follow">
+                <a href="https://docsend.com/view/2wt4ivv8e3r9dfnr" target="_blank" className="diversify-follow">
                     <svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" viewBox="0 0 13 13" fill="none">
                         <path d="M11.0037 3.41421L2.39712 12.0208L0.98291 10.6066L9.5895 2H2.00373V0H13.0037V11H11.0037V3.41421Z" fill="white"/>
                     </svg>
